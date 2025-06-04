b321e54b92e94f64abf2806451aeb08cb152819c
Celso Neto <celsoneto95@hotmail.com>
2025-06-03 23:32:45 -0300

    feat: criar DTO e mapper para Funcionario

--- src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioDTO.java
+++ src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioDTO.java
@@ -1,4 +1,29 @@
 package br.com.celsoneto.GerenciadorDeFuncionarios.Funcionarios;
 
+import br.com.celsoneto.GerenciadorDeFuncionarios.Lojas.LojaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioDTO {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private String estadoCivil;
+    private CargoFuncionario cargo;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataUpdate;
+    private LojaModel lojas;
+
 }
--- src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioMapper.java
+++ src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioMapper.java
@@ -1,4 +1,43 @@
 package br.com.celsoneto.GerenciadorDeFuncionarios.Funcionarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FuncionarioMapper {
+
+    public FuncionarioModel map(FuncionarioDTO funcionarioDTO){
+
+        FuncionarioModel funcionarioModel = new FuncionarioModel();
+        funcionarioModel.setId(funcionarioDTO.getId());
+        funcionarioModel.setNome(funcionarioDTO.getNome());
+        funcionarioModel.setCpf(funcionarioDTO.getCpf());
+        funcionarioModel.setEmail(funcionarioDTO.getEmail());
+        funcionarioModel.setTelefone(funcionarioDTO.getTelefone());
+        funcionarioModel.setDataNascimento(funcionarioDTO.getDataNascimento());
+        funcionarioModel.setEstadoCivil(funcionarioDTO.getEstadoCivil());
+        funcionarioModel.setCargo(funcionarioDTO.getCargo());
+        funcionarioModel.setDataCadastro(funcionarioDTO.getDataCadastro());
+        funcionarioModel.setDataUpdate(funcionarioDTO.getDataUpdate());
+        funcionarioModel.setLojas(funcionarioDTO.getLojas());
+
+        return funcionarioModel;
+    }
+
+    public FuncionarioDTO map(FuncionarioModel funcionarioModel){
+
+        FuncionarioDTO funcionarioDTO = new FuncionarioDTO();
+        funcionarioDTO.setId(funcionarioModel.getId());
+        funcionarioDTO.setNome(funcionarioModel.getNome());
+        funcionarioDTO.setCpf(funcionarioModel.getCpf());
+        funcionarioDTO.setEmail(funcionarioModel.getEmail());
+        funcionarioDTO.setTelefone(funcionarioModel.getTelefone());
+        funcionarioDTO.setDataNascimento(funcionarioModel.getDataNascimento());
+        funcionarioDTO.setEstadoCivil(funcionarioModel.getEstadoCivil());
+        funcionarioDTO.setCargo(funcionarioModel.getCargo());
+        funcionarioDTO.setDataCadastro(funcionarioModel.getDataCadastro());
+        funcionarioDTO.setDataUpdate(funcionarioModel.getDataUpdate());
+        funcionarioDTO.setLojas(funcionarioModel.getLojas());
+
+        return funcionarioDTO;
+    }
 }

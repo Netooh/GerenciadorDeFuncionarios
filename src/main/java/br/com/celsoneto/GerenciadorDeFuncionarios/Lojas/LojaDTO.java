6e87ddf4ca76ba89c76efb76181513481a0ad64a
Celso Neto <celsoneto95@hotmail.com>
2025-06-07 22:25:39 -0300

    feat: criar DTO e mapper para Loja

--- src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Lojas/LojaDTO.java
+++ src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Lojas/LojaDTO.java
@@ -1,4 +1,21 @@
 package br.com.celsoneto.GerenciadorDeFuncionarios.Lojas;
 
+import br.com.celsoneto.GerenciadorDeFuncionarios.Funcionarios.FuncionarioModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LojaDTO {
+
+    private Long id;
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private List<FuncionarioModel> funcionarios;
+
 }
--- src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Lojas/LojaMapper.java
+++ src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Lojas/LojaMapper.java
@@ -1,4 +1,31 @@
 package br.com.celsoneto.GerenciadorDeFuncionarios.Lojas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LojaMapper {
+
+    public LojaModel map(LojaDTO lojaDTO){
+
+        LojaModel lojaModel = new LojaModel();
+        lojaModel.setId(lojaDTO.getId());
+        lojaModel.setNome(lojaDTO.getNome());
+        lojaModel.setCnpj(lojaDTO.getCnpj());
+        lojaModel.setEndereco(lojaDTO.getEndereco());
+        lojaModel.setFuncionarios(lojaDTO.getFuncionarios());
+
+        return lojaModel;
+    }
+
+    public LojaDTO map(LojaModel lojaModel){
+
+        LojaDTO lojaDTO = new LojaDTO();
+        lojaDTO.setId(lojaModel.getId());
+        lojaDTO.setNome(lojaModel.getNome());
+        lojaDTO.setCnpj(lojaModel.getCnpj());
+        lojaDTO.setEndereco(lojaModel.getEndereco());
+        lojaDTO.setFuncionarios(lojaModel.getFuncionarios());
+
+        return lojaDTO;
+    }
 }

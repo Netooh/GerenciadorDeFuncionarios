f7679e4d0c1af3fb2d1e8bf06a657e8b19ed107f
Celso Neto <celsoneto95@hotmail.com>
2025-06-11 22:16:21 -0300

    feat: adicionar lógica para cadastrar funcionários no controller

--- src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioControllerUi.java
+++ src/main/java/br/com/celsoneto/GerenciadorDeFuncionarios/Funcionarios/FuncionarioControllerUi.java
@@ -1,8 +1,10 @@
 package br.com.celsoneto.GerenciadorDeFuncionarios.Funcionarios;
 
+import br.com.celsoneto.GerenciadorDeFuncionarios.Lojas.LojaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -11,9 +13,11 @@ import java.util.List;
 public class FuncionarioControllerUi {
 
     private final FuncionarioService funcionarioService;
+    private final LojaService lojaService;
 
-    public FuncionarioControllerUi(FuncionarioService funcionarioService) {
+    public FuncionarioControllerUi(FuncionarioService funcionarioService, LojaService lojaService) {
         this.funcionarioService = funcionarioService;
+        this.lojaService = lojaService;
     }
 
     @GetMapping("/inicio")
@@ -35,6 +39,22 @@ public class FuncionarioControllerUi {
             return "html/gerenciadorDeFuncionarios";
         }
     }
+
+    @GetMapping("/cadastrar")
+    public String mostrarFormularioFuncionario(Model model) {
+        model.addAttribute("funcionario", new FuncionarioDTO());
+        model.addAttribute("cargos", CargoFuncionario.values());
+        model.addAttribute("lojas", lojaService.listarLojas());
+        return "html/cadastrarFuncionario";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarFuncionario(@ModelAttribute FuncionarioDTO funcionario, RedirectAttributes redirectAttributes) {
+        funcionarioService.cadastrarFuncionario(funcionario);
+        redirectAttributes.addFlashAttribute("mensagem", "Funcionário cadastrado com sucesso!");
+        return "redirect:/funcionarios/ui/inicio";
+    }
+
 
     @GetMapping("/deletar/{id}")
     public String deletarFuncionarioID(@PathVariable Long id){
